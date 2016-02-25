e24e9790398e11e91d2dcb648830ec65e4e422f5
Bluek404 <iovxw@outlook.com>
2016-02-24 21:02:45 -0600

    http server

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [org.clojure/core.async "0.2.374"]
                  [hiccup "1.0.5"] ; html 模板
                  [compojure "1.4.0"] ; http 路由
-                 [aleph "0.4.1-beta4"] ; http 框架
+                 [javax.servlet/servlet-api "2.5"]
+                 [http-kit "2.1.19"] ; http 框架
                  [me.raynes/conch "0.8.0"] ; 命令执行
                  [de.mkammerer/argon2-jvm "1.1"] ; password hashing
                  [org.clojure/data.json "0.2.6"] ; json
--- src/ejnkrfsd/core.clj
+++ src/ejnkrfsd/core.clj
@@ -1,9 +1,23 @@
 (ns ejnkrfsd.core
   (:require [ejnkrfsd.steam :as steam]
-            [clojure.java.io :as io]
+            [org.httpkit.server :refer :all]
+            [compojure.core :refer [defroutes GET POST DELETE ANY context]]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
             [clojure.data.json :as json]
+            [clojure.core.async :refer [go]]
             [me.raynes.conch.low-level :as sh])
   (:gen-class))
+
+(defn hello [req]
+  (with-channel req channel
+    (send! channel {:status 200
+                    :headers {"Content-Type" "text/plain"}
+                    :body    "hello"})))
+
+(defroutes all-routes
+  (GET "/" [] hello)
+  (route/not-found "404"))
 
 (defn -main [& args]
   (let [config (-> (if-let [cfg-file (first args)]
@@ -14,4 +28,5 @@
         database {:classname   "org.sqlite.JDBC"
                   :subprotocol "sqlite"
                   :subname     (str (config :workdir) "database.db")}]
-    (prn config)))
+    (prn config)
+    (run-server (site #'all-routes) {:port (config :port)})))
